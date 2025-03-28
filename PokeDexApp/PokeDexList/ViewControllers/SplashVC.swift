7a5b120f5bd32e6084c5470b3d2b0c1c051727f1
Maximo Hinojosa <maximohinojosa13@gmail.com>
2025-03-27 21:12:38 -0400

    Created SplashVC

---
+++ PokeDexApp/PokeDexList/ViewControllers/SplashVC.swift
@@ -0,0 +1,17 @@
+//
+//  SplashVC.swift
+//  PokeDexApp
+//
+//  Created by Maximo Hinojosa on 3/27/25.
+//
+
+import UIKit
+
+
+class SplashVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
