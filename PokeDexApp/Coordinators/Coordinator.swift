15fa5a4ee5d369ed0a38b9b972519cc0b30ba2c0
Maximo Hinojosa <maximohinojosa13@gmail.com>
2025-03-27 22:35:13 -0400

    created ApplicationCoordinator file

--- PokeDexApp/Coordinators/Coordinator.swift
+++ PokeDexApp/Coordinators/Coordinator.swift
@@ -20,27 +20,3 @@ protocol Coordinator {
     /// Starts the coordinator.
     func start()
 }
-
-/// `ApplicationCoordinator` is responsible for managing the application's main window and navigation flow.
-/// It initializes the root view controller and starts the initial coordinator.
-class ApplicationCoordinator: Coordinator {
-    private let window: UIWindow
-    var childCoordinators: [Coordinator] = []
-    var navigationController: UINavigationController
-    
-    /// Initializes a new instance of `ApplicationCoordinator` with the given window.
-    /// - Parameter window: The main window of the application.
-    init(window: UIWindow) {
-        self.window = window
-        self.navigationController = UINavigationController()
-        self.window.rootViewController = navigationController
-    }
-    
-    /// Starts the application by initializing and starting the `SplashCoordinator`.
-    func start() {
-        let splashCoordinator = SplashCoordinator(navigationController: navigationController)
-        splashCoordinator.start()
-        window.makeKeyAndVisible()
-    }
-}
-
