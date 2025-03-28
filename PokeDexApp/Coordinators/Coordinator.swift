8674f16dabea73dadea8163c02a00ef448602330
Maximo Hinojosa <maximohinojosa13@gmail.com>
2025-03-27 22:33:11 -0400

    Created documentation for Coordinator & Application Coordinator implementation

--- PokeDexApp/Coordinators/Coordinator.swift
+++ PokeDexApp/Coordinators/Coordinator.swift
@@ -8,24 +8,35 @@
 import UIKit
 
 
+/// `Coordinator` is a protocol that defines the basic structure for coordinators in the application.
+/// Coordinators are responsible for managing navigation flow and child coordinators.
 protocol Coordinator {
+    /// An array to keep track of child coordinators.
     var childCoordinators: [Coordinator] { get set }
+    
+    /// The navigation controller used for navigation.
     var navigationController: UINavigationController { get set }
+    
+    /// Starts the coordinator.
     func start()
 }
 
-// MARK: Application Coordinator
+/// `ApplicationCoordinator` is responsible for managing the application's main window and navigation flow.
+/// It initializes the root view controller and starts the initial coordinator.
 class ApplicationCoordinator: Coordinator {
     private let window: UIWindow
     var childCoordinators: [Coordinator] = []
     var navigationController: UINavigationController
     
+    /// Initializes a new instance of `ApplicationCoordinator` with the given window.
+    /// - Parameter window: The main window of the application.
     init(window: UIWindow) {
         self.window = window
         self.navigationController = UINavigationController()
         self.window.rootViewController = navigationController
     }
     
+    /// Starts the application by initializing and starting the `SplashCoordinator`.
     func start() {
         let splashCoordinator = SplashCoordinator(navigationController: navigationController)
         splashCoordinator.start()
