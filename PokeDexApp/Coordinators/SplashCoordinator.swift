9a9f0be4e70c849d8bc1dd87c31f0d4da6688d6a
Maximo Hinojosa <maximohinojosa13@gmail.com>
2025-03-27 22:39:49 -0400

    documentation for SplashCoordinator

--- PokeDexApp/Coordinators/SplashCoordinator.swift
+++ PokeDexApp/Coordinators/SplashCoordinator.swift
@@ -7,16 +7,20 @@
 
 import UIKit
 
-
+/// `SplashCoordinator` is responsible for managing the navigation flow during the splash screen.
+/// It checks if there are stored Pokemon details and navigates accordingly.
 class SplashCoordinator: Coordinator {
     var childCoordinators: [Coordinator] = []
     var navigationController: UINavigationController
     let dataManager = PokemonDataManager.shared
     
+    /// Initializes a new instance of `SplashCoordinator` with the given navigation controller.
+    /// - Parameter navigationController: The navigation controller used for navigation.
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
     
+    /// Starts the splash coordinator by checking for stored Pokemon details and navigating accordingly.
     func start() {
         if dataManager.hasStoredObjects() {
             let pokemonDetailModels = dataManager.getAllPokemonDetails()
