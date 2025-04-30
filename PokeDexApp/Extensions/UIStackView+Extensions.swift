cd93f9f115f5466fcae666086a7c5ab1535dd920
Maximo Hinojosa <maximohinojosa13@gmail.com>
2025-04-30 18:48:14 -0400

    [Create] - stack view extension

---
+++ PokeDexApp/Extensions/UIStackView+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIStackView+Extensions.swift
+//  PokeDexApp
+//
+//  Created by Maximo Hinojosa on 4/25/25.
+//
+
+import UIKit
+
+extension UIStackView {
+    /// Adds an array of views to the arrangedSubviews of this stack view.
+    ///
+    /// - Parameter views: The views to add as arranged subviews.
+    func addArrangedSubviews(_ views: [UIView]) {
+        views.forEach { addArrangedSubview($0) }
+    }
+}
