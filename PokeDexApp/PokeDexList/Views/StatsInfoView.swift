f5a9df97c3768bb1e8c38879b61c0f707ff25609
Maximo Hinojosa <maximohinojosa13@gmail.com>
2025-05-01 13:48:44 -0400

    [Update] - stats UI

--- PokeDexApp/PokeDexList/Views/StatsInfoView.swift
+++ PokeDexApp/PokeDexList/Views/StatsInfoView.swift
@@ -11,16 +11,20 @@ import UIKit
 class StatsInfoView: UIStackView {
 
     struct UIModel {
-        let name: String
-        let baseValue: Int
-        let minValue: Int
-        let maxValue: Int
+        struct Stat {
+            let name: String
+            let baseValue: Int
+            let minValue: Int
+            let maxValue: Int
+        }
+        let themeColor: String
+        let stats: [Stat]
     }
 
-    init(model: [UIModel]) {
+    init(model: UIModel) {
         super.init(frame: .zero)
         axis = .vertical
-        spacing = 12
+        spacing = 20
         setupStats(model)
     }
 
@@ -28,40 +32,55 @@ class StatsInfoView: UIStackView {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func setupStats(_ model: [UIModel]) {
-        model.forEach { stat in
+    private func setupStats(_ model: UIModel) {
+        let container = UIStackView()
+        container.axis = .vertical
+        container.spacing = 12
+        
+        model.stats.forEach { stat in
             let statRow = createStatRow(stat: stat)
-            addArrangedSubview(statRow)
+            container.addArrangedSubview(statRow)
         }
+        let titleLabel = PDLabel(text: "Base Stats", textColor: PokemonBackgroundColor(model.themeColor), fontWeight: .semiBold, fontSize: 18)
+        
+        addArrangedSubviews([
+            titleLabel,
+            container
+        ])
     }
 
-    private func createStatRow(stat: UIModel) -> UIView {
+    private func createStatRow(stat: UIModel.Stat) -> UIView {
         let container = UIStackView()
         container.axis = .horizontal
         container.spacing = 12
         container.alignment = .center
+        container.distribution = .fill
 
-        let nameLabel = UILabel()
-        nameLabel.text = stat.name
-        nameLabel.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
-        nameLabel.textColor = .darkGray
-        nameLabel.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        let nameLabel = PDLabel(text: stat.name, textColor: .black, fontWeight: .regular, fontSize: 14)
+        nameLabel.widthAnchor.constraint(equalToConstant: 60).isActive = true
 
-        let valueLabel = UILabel()
-        valueLabel.text = "\(stat.baseValue)"
-        valueLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        valueLabel.textColor = .black
-        valueLabel.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        let valueLabel = PDLabel(text: "\(stat.baseValue)", textColor: .black, fontWeight: .regular, fontSize: 14)
 
         let progressBar = UIProgressView(progressViewStyle: .default)
         progressBar.progressTintColor = UIColor.systemGreen.withAlphaComponent(0.6)
         progressBar.trackTintColor = UIColor.systemGray6
         progressBar.setProgress(Float(stat.baseValue) / 100.0, animated: false)
 
-        container.addArrangedSubview(nameLabel)
-        container.addArrangedSubview(valueLabel)
-        container.addArrangedSubview(progressBar)
-
+        
+        let minLabel = PDLabel(text: "\(stat.minValue)", textColor: .black, fontWeight: .regular, fontSize: 14)
+        minLabel.contentMode = .right
+        
+        let maxLabel = PDLabel(text: "\(stat.maxValue)", textColor: .black, fontWeight: .regular, fontSize: 14)
+        maxLabel.contentMode = .right
+        
+        container.addArrangedSubviews([
+            nameLabel,
+            valueLabel,
+            progressBar,
+            minLabel,
+            maxLabel
+        ])
+        
         return container
     }
 }
